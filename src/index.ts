e69c256c18f9d0020efceeaa55908329c0f1067f
Nakroma <tarackobarack@gmail.com>
2022-09-22 18:30:14 +0200

    Added basic test setup

--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,50 @@ import sharp from 'sharp';
 import path from 'path';
 import fs from 'fs';
 
-export default async function handler(req: Request, res: Response, basePath: string) {
-    // Base vars
-    const width = req.query.width;
-    const filePath = req.path.split('/');
-    const file = filePath.pop();
-    if (!file) return res.status(400).send('Invalid request path.');
+export default function handler(basePath: string) {
+    // Wrapper function to return express handler
+    return async (req: Request, res: Response) => {
+        // Base vars
+        const width = (Array.isArray(req.query.width) ? req.query.width[0] : req.query.width) as string | undefined;
+        const filePath = req.path.split('/');
+        const file = filePath.pop();
+        if (!file) return res.status(400).send('Invalid request path.');
 
-    // Generate filename
-    const fileSplit = file.split('.');
-    if (width) fileSplit[0] += `_w${width}`;
-    const fileName = fileSplit.join('.');
+        // Generate filename
+        const fileSplit = file.split('.');
+        if (width) fileSplit[0] += `_w${width}`;
+        const fileName = fileSplit.join('.');
 
-    // File paths for original and (possibly) new file
-    const originalFilePath = path.join(basePath, ...filePath, file);
-    const outputFilePath = path.join(basePath, ...filePath, fileName);
-
-    try {
-        // Check if file exists and send if it does
-        await fs.promises.access(outputFilePath, fs.constants.F_OK);
-        return res.sendFile(outputFilePath);
-    } catch (err) {
-        // File does not exist
-        if (!width) return res.status(404).send('File not found.');
+        // File paths for original and (possibly) new file
+        const originalFilePath = path.join(basePath, ...filePath, file);
+        const outputFilePath = path.join(basePath, ...filePath, fileName);
 
         try {
-            // Try to create it, otherwise 404 if there's no original file
-            const buffer = await sharp(originalFilePath)
-                .resize({ width: Number(width) })
-                .toBuffer();
-
-            // Send buffer
-            res.writeHead(200, {
-                'Content-Type': `image/${fileSplit.slice(-1)}`,
-                'Content-Length': buffer.length,
-            });
-            res.end(buffer);
-
-            // Write buffer to file
-            return fs.promises.writeFile(outputFilePath, buffer);
+            // Check if file exists and send if it does
+            await fs.promises.access(outputFilePath, fs.constants.F_OK);
+            return res.sendFile(outputFilePath);
         } catch (err) {
-            return res.status(404).send('File not found.');
+            // File does not exist
+            if (!width) return res.status(404).send('File not found.');
+
+            try {
+                // Try to create it, otherwise 404 if there's no original file
+                const buffer = await sharp(originalFilePath)
+                    .resize({ width: Number(width) })
+                    .toBuffer();
+
+                // Send buffer
+                res.writeHead(200, {
+                    'Content-Type': `image/${fileSplit.slice(-1)[0]}`,
+                    'Content-Length': buffer.length,
+                });
+                res.end(buffer);
+
+                // Write buffer to file
+                return fs.promises.writeFile(outputFilePath, buffer);
+            } catch (err) {
+                return res.status(404).send('File not found.');
+            }
         }
     }
 }
---
+++ test/index.test.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import sharp from 'sharp';
+import path from 'path';
+import http from 'http';
+import fs from 'fs';
+import handler from '../src/index';
+
+let app = {} as express.Application;
+let server = {} as http.Server;
+const BASE_PATH = './test';
+const TEST_FILE = 'test.png';
+
+// Hooks
+beforeAll((done) => {
+    // Create test image
+    sharp({
+        create: {
+            width: 200,
+            height: 100,
+            channels: 4,
+            background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+        }
+    })
+        .png()
+        .toFile(path.join(BASE_PATH, TEST_FILE))
+        .then(() => {
+            // Prepare express app
+            app = express();
+            app.get('*', handler(BASE_PATH));
+            server = app.listen(3000, done);
+        });
+})
+afterAll(async () => {
+    // Cleanup
+    await server.close();
+    fs.rmSync(path.join(BASE_PATH, TEST_FILE));
+})
+
+test('test test', () => {
+    expect(true).toBe(true);
+})
